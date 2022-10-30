3d0cac2cbe25cbb65ab69394cd9070e176aa93ac
marioborrego <maro.borrego100@gmail.com>
2022-10-30 19:25:58 +0100

    Data Binding: Vinculación en dos direcciones, Data Binding Adapter

--- app/src/main/java/com/marioborrego/cupones/mainModule/viewModel/MainViewModel.kt
+++ app/src/main/java/com/marioborrego/cupones/mainModule/viewModel/MainViewModel.kt
@@ -23,7 +23,7 @@ class MainViewModel: ViewModel() {
         coupon.value?.code?.let { code ->
             viewModelScope.launch {
                 hideKeyboard.value = true
-                coupon.value = repository.consultCouponByCode(code)
+                coupon.value = repository.consultCouponByCode(code) ?: CouponEntity(code = code, isActive = false)
             }
         }
     }
@@ -33,6 +33,7 @@ class MainViewModel: ViewModel() {
             viewModelScope.launch {
                 hideKeyboard.value = true
                 try {
+                    couponEntity.isActive = true
                     repository.saveCoupon(couponEntity)
                     consultCouponByCode()
                     snackbarMsg.value = R.string.main_save_success
